eb23b85d0fc768e747182e79848c298c432ef05d
gfc_vb_ub2004 <100248345+GrapefruitCat030@users.noreply.github.com>
2025-03-31 00:11:44 +0800

    modified:   .gitignore

--- seeds/1_seed.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-arr1=(1 "2" 3)
-diff <(echo ${arr1[1]}) <(echo "2") | tee output.txt
-
--- seeds/2_seed.sh
+++
@@ -1,8 +0,0 @@
-arr1=(1 "b" 3)
-len=${#arr1[@]}
-for i in ${arr1[@]};
-do 
-	echo ${arr1[1]}
-done
-arr2=(4 5)
-echo ${arr2[0]}
