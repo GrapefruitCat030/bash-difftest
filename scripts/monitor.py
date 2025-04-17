eb5d39bb758f34fb444680f7a38e372ce8e46749
gfc_vb_ub2004 <100248345+GrapefruitCat030@users.noreply.github.com>
2025-04-17 22:32:42 +0800

    to: fix the monitor script to detect the psycho shell

--- scripts/monitor.py
+++ scripts/monitor.py
@@ -2,41 +2,40 @@
 
 # execute: nohup python3 monitor.py > monitor.log 2>&1 &
 
+import os
 import psutil
 import time
 from datetime import datetime
 
 # config
 CPU_THRESHOLD = 90
-MIN_LIFETIME = 10
-CHECK_INTERVAL = 5
+MIN_LIFETIME = 15
+CHECK_INTERVAL = 10
 
 shell_lst = [
-    "/home/njucs/project/bash-difftest/shell/dash-0.5.12/dash",
-    "/home/njucs/project/bash-difftest/shell/bash-5.1/bash",
+    "/workspace/shell/dash-0.5.12/src/dash",
+    "/workspace/shell/bash-5.1/bash",
 ]
 
 def is_shell_proc(proc):
     try:
-        return proc.exe() in shell_lst
+        exe_path = os.path.realpath(proc.exe())
+        return exe_path in shell_lst
     except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
         return False
 
 def monitor():
     while True:
         now = time.time()
-        for proc in psutil.process_iter(['pid', 'name', 'create_time']):
+        for proc in psutil.process_iter(['pid', 'name', 'create_time', 'exe']):
             if not is_shell_proc(proc):
                 continue
             try:
-                print(is_shell_proc(proc))
-                print(proc)
-
                 lifetime = now - proc.info['create_time']
                 if lifetime < MIN_LIFETIME:
                     continue
                 
-                print(f"[{datetime.now()}] Kill {proc.name()}(pid={proc.pid}) "
+                print(f"[{datetime.now()}] Kill {proc.name()} {proc.cmdline()} (pid={proc.pid} exe={proc.exe()}) "
                         f"lifetime={lifetime:.1f}s")
                 proc.kill()
             
